d8d9f7dc300bbfe2a2d890d270e58963cd9d1757
ilysem <ilysem@hawaii.edu>
2025-10-07 11:33:16 -1000

    fixed the drawer nave bug

--- js/script.js
+++ js/script.js
@@ -7,13 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
     menubutton.onclick = () => {
     
         // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
-        if (sitenav.getAttribute("data-menustate") === "closed" ) {
-            sitenav.setAttribute("data-menustate", "open");
-        } else {
+        if (sitenav.getAttribute("data-menustate") === "open" ) {
             sitenav.setAttribute("data-menustate", "closed");
+        } else {
+            sitenav.setAttribute("data-menustate", "open");
         };
     
     };
-
+    // REMOVE THE DATA-STATE ON RESIZE 
+    window.onresize = () => {
+     sitenav.removeAttribute("data-menustate");
+    };
+    
 
 });
