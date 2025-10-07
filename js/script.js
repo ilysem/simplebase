e21656e58797590e0a6eee202727915881df0953
ilysem <ilysem@hawaii.edu>
2025-10-07 12:02:47 -1000

    fixed a bug

--- js/script.js
+++ js/script.js
@@ -5,19 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const menubutton = document.querySelector(".menubutton");
 
     menubutton.onclick = () => {
-    
         // IF MENU IS CLOSED, OPEN IT, ELSE CLOSE IT
         if (sitenav.getAttribute("data-menustate") === "open" ) {
             sitenav.setAttribute("data-menustate", "closed");
         } else {
             sitenav.setAttribute("data-menustate", "open");
         };
-    
     };
-    // REMOVE THE DATA-STATE ON RESIZE 
+
+    // Remove the data-attribute on resize to fix drawer nav animation
     window.onresize = () => {
-     sitenav.removeAttribute("data-menustate");
+        sitenav.removeAttribute("data-menustate");
     };
-    
 
 });
